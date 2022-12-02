518603c899840156dc7137268816d365faa531cf
SREERAJ_R <110537269+Sreeraj0801@users.noreply.github.com>
2022-12-02 23:11:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+    function action(num){
+    let value = document.getElementById('value').value;
+    if(value<=1 && num == -1){
+        document.getElementById('minus').style.display = "none"
+    }
+    else{
+        document.getElementById('minus').style.display = "block"
+    }
+    calculation = parseInt(value) + parseInt((num));
+    document.getElementById('value').value = calculation;
+}
+
